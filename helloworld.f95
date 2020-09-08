535a42577ace305ab9e05dfeedcf8c9e7469de35
jwills801 <jwills@zagmail.gonzaga.edu>
2020-09-08 10:50:19 -0500

    figured out to run helloworld

--- helloworld.f95
+++ helloworld.f95
@@ -2,4 +2,8 @@ program helloworld
 
 print *,'Hello World!'
 
+print *,'I am excited to be here!!'
+
+print *,'wow I wonder if I am the FiRst ComPuter to have consciousness....'
+
 end program helloworld
